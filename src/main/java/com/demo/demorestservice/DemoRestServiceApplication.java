e60ad51ab17949b3106a2a1d368a8fa012e2b9ba
patrick wafo <wafo.digital@gmail.com>
2024-01-30 17:02:39 +0100

    add Entry Application Class that also define the app api path

---
+++ src/main/java/com/demo/demorestservice/DemoRestServiceApplication.java
@@ -0,0 +1,9 @@
+package com.demo.demorestservice;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class DemoRestServiceApplication extends Application {
+
+}
